1130da4330356887dd87ba88f6690b29510e79f8
Владимир Стальмаков <akummail@gmail.com>
2017-07-06 00:41:58 +0300

    Простенький фильтр

--- climate/THSensors.cpp
+++ climate/THSensors.cpp
@@ -3,6 +3,11 @@
 THSensors::THSensors(int interval)
         : IntervalWorckerBase(interval) {
 
+    for (int i = 0; i < RANGE_FILTER; i++) {
+        tFilter[i] = 0;
+        hFilter[i] = 0;
+    }
+
     bool status = bme.begin(BME_280_ADDRESS);
     if (!status) {
         Serial.println("Could not find a valid BME280 sensor, check wiring!");
@@ -10,7 +15,24 @@ THSensors::THSensors(int interval)
 }
 
 void THSensors::work(Values *values, unsigned long currentMillis) {
-    values->getClimatVal(HUMIDITY)->setCurrent(bme.readHumidity());
-    values->getClimatVal(TEMPERATURE)->setCurrent(bme.readTemperature());
+    values->getClimatVal(HUMIDITY)->setCurrent(_getVal(hFilter,bme.readHumidity()));
+    values->getClimatVal(TEMPERATURE)->setCurrent(_getVal(tFilter,bme.readTemperature()));
     // bme.readPressure() / 100.0F
+}
+
+float THSensors::_getVal(float *values, float current) {
+    for (int i = 0; i < RANGE_FILTER; i++) {
+        values[i] = values[i + 1];
+    }
+    values[RANGE_FILTER - 1] = current;
+
+    float result = 0;
+    int count = 0;
+    for (int i = 0; i < RANGE_FILTER; i++) {
+        if (values[i] > 0) {
+            result += values[i];
+            count++;
+        }
+    }
+    return result / count;
 }
--- climate/THSensors.hpp
+++ climate/THSensors.hpp
@@ -10,13 +10,16 @@
 class THSensors : public IntervalWorckerBase {
 #define BME_280_ADDRESS 0x76
     Adafruit_BME280 bme;
+#define RANGE_FILTER 5
 
+    float hFilter[RANGE_FILTER];
+    float tFilter[RANGE_FILTER];
 public:
 
     THSensors(int interval);
 
     void work(Values *values, unsigned long currentMillis) override;
-
+float _getVal(float values[], float current);
 };
 
 #endif
